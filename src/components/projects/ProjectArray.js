aa785af32a328129853a1029d58faef20615bf9b
Nicholas <atok948@gmail.com>
2024-09-04 11:04:11 +0000

    Update Projects

---
+++ src/components/projects/ProjectArray.js
@@ -0,0 +1,101 @@
+const projects = [
+  {
+    id: 1,
+    title: "Study Buddy",
+    image:
+      "https://ibb.co/VThtvNZ",
+    description:
+      "StudyBuddy is a personal assistant created for students to help them manage their life in school.",
+    languages: ["react", "firebase", "bootstrap"],
+    demo: "https://walking-buddy-636cb.web.app/",
+    source: "https://github.com/atok624/StudyBuddy",
+  },
+  {
+    id: 2,
+    title: "Purpool",
+    image:
+      "https://ibb.co/rvcHJBy",
+    description:
+      "This project is a modern, user-friendly chat application built with React and Firebase",
+    languages: ["react", "firebase", "tailwind"],
+    demo: "https://github.com/atok624/Purpool-Chat-Application",
+    source: "https://github.com/atok624/Purpool-Chat-Application",
+  },
+  {
+    id: 3,
+    title: "Hotel Booking",
+    image:
+      "https://res.cloudinary.com/dnkzdjigk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1691540710/Capture_adstck.png",
+    description:
+      "The Frugalize app helps you to manage your budget. The app allows users to create categories and add transactions to the categories.",
+    languages: ["ruby", "ruby-on-rails", "postgresql"],
+    demo: "https://frugalize.onrender.com/",
+    source: "https://github.com/atok624/book-a-room-frontend.git",
+  },
+  {
+    id: 4,
+    title: " Budget-App",
+    image:
+      "https://res.cloudinary.com/dnkzdjigk/image/upload/v1691541274/Capture1_jkjzjd.png",
+    description:
+      "Budget App: Web app for personal finance management. Set budgets, track spending in categories like groceries, transportation, and entertainment. Make informed financial decisions for a balanced budget.",
+    languages: ["react", "redux", "rest-api", "tailwindcss"],
+    demo: "https://budget-app-frontend.onrender.com/",
+    source: "https://github.com/atok624/Budget-App.git",
+  },
+  {
+    id: 5,
+    title: "Space Travellers",
+    image:
+      "https://res.cloudinary.com/dnkzdjigk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1691541665/Capture2_cihuth.png",
+    description:
+      "Space Travelers Hub is web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.",
+    languages: [
+      "react",
+      "redux",
+      "restful api",
+      "webpack"
+    ],
+    demo: "https://space-travelers-hub-6t7j.onrender.com/",
+    source: "https://github.com/atok624/space_travelers-hub.git",
+  },
+  {
+    id: 6,
+    title: "LeaderBoard",
+    image:
+      "https://res.cloudinary.com/dnkzdjigk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1691542380/Capture3_tbt4hi.png",
+    description:
+      "The leaderboard website displays scores submitted by different players. It also allows you to submit your score. All data is preserved thanks to the external API",
+    languages: ["Javasript", "webpack", "restful api"],
+    demo: "https://leaderboard-xzry.onrender.com/",
+    source: "https://github.com/atok624/Leaderboard.git",
+  },
+  {
+    id: 7,
+    title: "Maths Magician",
+    image:
+      "https://res.cloudinary.com/dnkzdjigk/image/upload/v1691546587/Capture4_qutvxn.png",
+    description:
+      '"Math magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations. Read a random math-related quote."',
+    languages: [
+      "javascript",
+      "restful api",
+      "bootstrap",
+    ],
+    demo: "https://math-magician-5re0.onrender.com/",
+    source: "https://github.com/atok624/maths_magicians.git",
+  },
+  {
+    id: 8,
+    title: "Timbuktu Institute",
+    image:
+      "https://res.cloudinary.com/dnkzdjigk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1691547184/Capture5_egxnxk.png",
+    description:
+      'Timbuktu Institute  is a program made with Html, CSS and JavaScript. It comprises of two pages of Desktop and two pages of mobile.',
+    languages: ["JavaScript", "tailwindcss", "HTML",],
+    demo: "https://capstone-uuaa.onrender.com/index.html",
+    source: "https://github.com/atok624/Capstone_Project1.git",
+  },
+];
+
+export default projects;
